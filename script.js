c27181f6f6f9c201a6020ddf16f69c3a35b74594
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-02-01 06:37:19 +0800

    feat: allow values to be assigned to a variable
    Button listeners were added inside a function to have a conditional statement
    evaluate which variable a value will be assigned.

--- script.js
+++ script.js
@@ -23,18 +23,35 @@ const display = document.querySelector('#display');
 const calc = document.querySelector('#Calculator');
 const operatorSymbol = document.querySelectorAll('.symbol');
 
-btns.forEach(btn => {
-  btn.addEventListener('click', function(e){
-    display.textContent = '';
-    firstNum += e.target.dataset.value;
-    display.textContent = firstNum;
-  })
-}
-)
+calculate();
 
 operatorSymbol.forEach(btn => {
   btn.addEventListener('click', function(e){
     operator = e.target.dataset.operator;
 
+    calculate();
   }); 
 } )
+//As for the moment, until such time that the code can be much more concise and efficient, resorted to  adding functionality to the buttons. By adding an event listeners inside a function, a conditional statement is implemented to which variable a value will be assigned.
+function calculate() {
+  if (firstNum == '' && operator == undefined) {
+    btns.forEach(btn => {
+      btn.addEventListener('click', function(e){
+        display.textContent = '';
+        firstNum += e.target.dataset.value;
+        display.textContent = firstNum;
+      })
+    }
+  )
+  } else {
+    btns.forEach(btn => {
+      btn.addEventListener('click', function(e){
+        display.textContent = '';
+        secondNum += e.target.dataset.value;
+        display.textContent = secondNum;
+      })
+    }
+  )
+
+  }
+}
