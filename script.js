22ec4a2c9cdfb79c72a28ec1e4148c336f378396
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-02-01 07:47:48 +0800

    fix: allow value of a var to be push to an array
    The display on the calculator indicates the correct value, however, the
    value of the second variable will concatenate on the first variable.
    
    So added a fix that will push the current value of a variable to an array.
    This prevents the value from being changed with every number button click.

--- script.js
+++ script.js
@@ -17,41 +17,52 @@ function divide (a, b) {
 let firstNum = '';
 let secondNum = '';
 let operator;
+let expression = [];
 
 const btns = document.querySelectorAll('.digit');
 const display = document.querySelector('#display');
 const calc = document.querySelector('#Calculator');
 const operatorSymbol = document.querySelectorAll('.symbol');
+const compute = document.querySelector('.compute');
 
-calculate();
+variableAssignment();
 
+//As the e.target.data.value cannot be reset to initial, decided to push the current value to an array
 operatorSymbol.forEach(btn => {
   btn.addEventListener('click', function(e){
     operator = e.target.dataset.operator;
+    expression.push(firstNum, operator);
 
-    calculate();
+    variableAssignment();
   }); 
 } )
-//As for the moment, until such time that the code can be much more concise and efficient, resorted to  adding functionality to the buttons. By adding an event listeners inside a function, a conditional statement is implemented to which variable a value will be assigned.
-function calculate() {
-  if (firstNum == '' && operator == undefined) {
+
+//As for the moment, until such time that the code can be much more concise and efficient, resorted to  adding functionality to the buttons. By adding an event listeners inside a function, a conditional statement is implemented to which variable a value will be assigned
+function variableAssignment() {
+  if (firstNum == '') {
     btns.forEach(btn => {
       btn.addEventListener('click', function(e){
         display.textContent = '';
         firstNum += e.target.dataset.value;
         display.textContent = firstNum;
+        console.log(firstNum);
       })
     }
   )
-  } else {
-    btns.forEach(btn => {
-      btn.addEventListener('click', function(e){
-        display.textContent = '';
-        secondNum += e.target.dataset.value;
-        display.textContent = secondNum;
-      })
-    }
-  )
-
+} else {
+  btns.forEach(btn => {
+    btn.addEventListener('click', function(e){
+      display.textContent = '';
+      secondNum += e.target.dataset.value;
+      display.textContent = secondNum;
+    })
   }
+)
 }
+}
+
+
+compute.addEventListener('click', function() {
+  expression.push(secondNum);
+  console.log(expression);
+})
