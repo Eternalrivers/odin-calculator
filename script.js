f7afd2b4346be20b2b0b54e2f73a00a1c25049e4
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-02-03 04:36:03 +0800

    feat: add function to evaluate an expression
    Added function that evaluates an expression that is in array form. The
    argument is the array, where the index[0] and index[2]. Where it then
    evaluates it using the operator sign using a conditional statement.

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ let firstNum = '';
 let secondNum = '';
 let operator;
 let expression = [];
+let result;
+
 
 const btns = document.querySelectorAll('.digit');
 const display = document.querySelector('#display');
@@ -32,24 +34,25 @@ operatorSymbol.forEach(btn => {
   btn.addEventListener('click', function(e){
     operator = e.target.dataset.operator;
     expression.push(firstNum, operator);
+    firstNum = '';
 
     variableAssignment();
   }); 
 } )
 
-//As for the moment, until such time that the code can be much more concise and efficient, resorted to  adding functionality to the buttons. By adding an event listeners inside a function, a conditional statement is implemented to which variable a value will be assigned
+//As for the moment, until such time that the code can be much more concise and efficient, resorted to  adding functionality )to the buttons. By adding an event listeners inside a function, a conditional statement is implemented to which variable a value will be assigned
 function variableAssignment() {
-  if (firstNum == '') {
+  if (expression.length == 0) {
     btns.forEach(btn => {
       btn.addEventListener('click', function(e){
         display.textContent = '';
-        firstNum += e.target.dataset.value;
+         firstNum += e.target.dataset.value;
         display.textContent = firstNum;
         console.log(firstNum);
       })
     }
   )
-} else {
+} else if (expression.length > 2 || expression.length == 2 ) {
   btns.forEach(btn => {
     btn.addEventListener('click', function(e){
       display.textContent = '';
@@ -61,8 +64,28 @@ function variableAssignment() {
 }
 }
 
-
 compute.addEventListener('click', function() {
   expression.push(secondNum);
+  secondNum = '';
   console.log(expression);
+  evalExpression(expression);
+  display.textContent = result;
 })
+
+function evalExpression(arr) {
+  let a = arr[0];
+  let b = arr[2];
+  
+  if (arr[1] == '*') {
+    return result = multiply (a, b);
+  }
+  if (arr[1] == '/') {
+    return result = divide (a, b);
+  }
+  if (arr[1] == '+') {
+    return result = add (Number(a), Number(b));
+  }
+  if (arr[1] == '-') {
+    return result = subtract (a, b);
+  }
+}
